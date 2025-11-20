de59b6d43b44b50733a278fcf4241ce206ace738
nourgaser <work@nourgaser.com>
2025-11-20 06:05:17 +0200

    lab5-prep: add ListingCard component

--- lab5-prep/src/components/ListingCard/index.jsx
+++ lab5-prep/src/components/ListingCard/index.jsx
@@ -5,7 +5,20 @@ const ListingCard = ({ listing, onDelete }) => {
 
   return (
     <div className="listing-card">
-      Listing content here.
+      <img src={image_url} className="card-image" alt={title} />
+      <div className="card-content">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <p>Price: {pricePerNight} EGP / night</p>
+        <div
+          className="delete-button"
+          onClick={() => {
+            onDelete(listing._id);
+          }}
+        >
+          Delete
+        </div>
+      </div>
     </div>
   );
 };
